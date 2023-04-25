f83b1d5674267d7d0da93dab14fe7feba7bd3608
CristianGonzalez07 <1995.cgonzalez.07@gmail.com>
2023-04-25 18:50:04 -0300

    colors and bg added to config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,25 @@
 module.exports = {
   content:["./src/**/*.{js,jsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sigmar: ['Sigmar One', 'cursive']
+      },
+      textColor: {
+        'primary': '#8332AC',
+        'primary-dark':"#240E2F",
+        'primary-light':"#A352CB"
+      },
+      backgroundColor:{
+        'primary': '#8332AC',
+      },
+      backgroundImage:{
+        'texture': "url('./src/assets/Texture.jpg')",
+      },
+      borderColor:{
+        'primary': '#8332AC'
+      }
+    },
   },
   plugins: [],
 }
