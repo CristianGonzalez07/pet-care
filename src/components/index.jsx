7e409d0e956e926bae005377aad182bec320f174
CristianGonzalez07 <1995.cgonzalez.07@gmail.com>
2023-04-25 18:51:58 -0300

    component export configured

---
+++ src/components/index.jsx
@@ -0,0 +1,13 @@
+import About from "./About";
+import Contact from "./Contact";
+import Footer from "./Footer"; 
+import Header from "./Header";
+import Services from "./Services";
+
+export {
+    About,
+    Contact,
+    Footer,
+    Header,
+    Services
+}
