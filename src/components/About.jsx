b53bbb0b63de3b0c406ad0b43316c2c0614e2207
CristianGonzalez07 <1995.cgonzalez.07@gmail.com>
2023-04-26 17:46:00 -0300

    modified responsive styles

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,9 +3,9 @@ import React from 'react'
 export default function About() {
   return (
     <div id="About" className='bg-primary h-[500px] w-full grid grid-cols-6'>
-      <div className='col-start-3 mt-10 col-span-3'>
-        <h1 className='text-white font-sigmar text-xl lg:text-5xl'>About us</h1>
-        <p className='mt-10 text-white font-sigmar text-xs lg:text-xl'>Donec feugiat lectus mi, nec scelerisque massa viverraac. Vivam vestibulum ornare imperdiet. Nam et sagittis diam. Maecenasma ximus fringilla viverra. Quisque malesuada enim sed aliquam feugiat. Curabitur id sem eu velit eleifend tincidunt.
+      <div className='col-start-2 mt-10 col-span-4'>
+        <h1 className='text-white font-sigmar text-xl md:text-3xl lg:text-5xl'>About us</h1>
+        <p className='mt-10 text-white font-sigmar text-xs md:text-lg lg:text-xl'>Donec feugiat lectus mi, nec scelerisque massa viverraac. Vivam vestibulum ornare imperdiet. Nam et sagittis diam. Maecenasma ximus fringilla viverra. Quisque malesuada enim sed aliquam feugiat. Curabitur id sem eu velit eleifend tincidunt.
 Curabitur et lorem et dolor iaculis convallis. Curabitur interdum ipsum convallis porttitor vulputate. Morbi sed massa at dui congue porttitor eget nec magna.</p>
       </div>
     </div>
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,26 +2,28 @@ import { Icon } from '@iconify/react';
 
 export default function Footer() {
   return (
-    <div className='h-[100px] bg-primary-dark grid grid-cols-3'>
+    <div className='h-[100px] bg-primary-dark grid md:grid-cols-3 grid-cols-1 pt-2 absolute w-full'>
       <div className='flex items-center justify-center'>
         <p className='text-white font-sigmar text-xs '>© 2023 All rights reserved.</p>
       </div>
       <div className='flex items-center justify-center'>
         <a href='https://www.instagram.com' target='_blank'>
-          <Icon className="inline h-[40px] w-[40px] text-white mr-2" icon="mdi:instagram"/>
+          <Icon className="inline md:h-[40px] md:w-[40px] text-white mr-2" icon="mdi:instagram"/>
         </a>
         <a href='https://www.facebook.com' target='_blank'>
-          <Icon className="inline h-[40px] w-[40px] text-white mr-2" icon="ic:baseline-facebook"/>
+          <Icon className="inline md:h-[40px] md:w-[40px] text-white mr-2" icon="ic:baseline-facebook"/>
         </a>
         <a href='https://www.twitter.com' target='_blank'>
-          <Icon className="inline h-[40px] w-[40px] text-white mr-2" icon="tabler:brand-twitter-filled"/>
+          <Icon className="inline md:h-[40px] md:w-[40px] text-white mr-2" icon="tabler:brand-twitter-filled"/>
         </a>
         <a href='https://www.youtube.com' target='_blank'>
-          <Icon className="inline h-[40px] w-[40px] text-white mr-2" icon="ri:youtube-fill"/>
+          <Icon className="inline h- md:h-[40px] md:w-[40px] text-white" icon="ri:youtube-fill"/>
         </a>      
       </div>
-      <div className='flex items-center justify-end mr-10'>
-        <button onClick={() => {
+      <div className='relative'>
+        <button 
+          className='absolute bottom-6 right-4'
+        onClick={() => {
           window.scrollTo({
             top: 0,
             behavior: "smooth"
