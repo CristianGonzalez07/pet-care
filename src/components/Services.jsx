11cd96a1589fceb123fcfc467e6712ceeacfa4c3
CristianGonzalez07 <1995.cgonzalez.07@gmail.com>
2023-04-28 19:01:46 -0300

    responsive fixed

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -32,7 +32,7 @@ export default function
         duration: 1,
       }} 
     >
-      <div className="w-[70%] grid grid-cols-3 gap-5"> 
+      <div className="w-[70%] grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5"> 
         {items.map(item => <Card value={item}/>)}
       </div>
     </motion.div>
